35884476967b710999609d746ee3c6dc570b8904
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-04-16 18:04:34 +0200

    Simple function that works

--- main.rs
+++ main.rs
@@ -4,13 +4,7 @@ fn main() {
 
   let it = fs::read_dir(".").unwrap();
 
-  let x = it.flat_map(|file| {
-    file.unwrap().file_name();
-  }).collect();
-
-  println!("{:?}", x);
-
   for f in it { 
     println!("{:?}", f.unwrap().file_name());
-  };
+  };  
 }
