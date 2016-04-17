6588eaafd1756ef1874ecba4a64c23e8c19d36af
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-04-17 22:01:16 +0200

    Add function to print a directory content without use debug

--- main.rs
+++ main.rs
@@ -1,10 +1,14 @@
 use std::fs;
 
+/// Print the `dir_path` dir.
+fn print_dir(dir: &str) -> () {
+  let it = fs::read_dir(dir).unwrap();
+
+  for f in it {
+    println!("{}", f.unwrap().file_name().into_string().unwrap());
+  }
+}
+
 fn main() {
-
-  let it = fs::read_dir(".").unwrap();
-
-  for f in it { 
-    println!("{:?}", f.unwrap().file_name());
-  };  
+  print_dir(".");
 }
