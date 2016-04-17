3276ab6f335d492986074932978a63d1f364fa77
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-04-17 22:12:27 +0200

    Add comments

--- main.rs
+++ main.rs
@@ -2,9 +2,11 @@ use std::fs;
 
 /// Print the `dir_path` dir.
 fn print_dir(dir: &str) -> () {
+  // The unwrap function extracts the Ok() value of the Result
   let it = fs::read_dir(dir).unwrap();
 
   for f in it {
+    // The into_string function will throw an error if it contains non-unicode characters
     println!("{}", f.unwrap().file_name().into_string().unwrap());
   }
 }
