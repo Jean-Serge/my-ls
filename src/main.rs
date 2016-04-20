8a1b636962876384c3d613b8b89d849a741930ee
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-04-20 23:07:59 +0200

    Win to make path argument run ... almost correctly

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,43 @@
 use std::fs;
 use std::env;
-use std::path::Path;
 
-struct Arguments<'a> {
-  path: &'a str
+// Use String for not having to know the string size
+struct Arguments {
+  path: String
 }
 
 /// Print the `dir_path` dir.
-fn print_dir(dir: &str) -> () {
+fn print_dir(dir: &String) -> () {
   // The unwrap function extracts the Ok() value of the Result
   let it = fs::read_dir(dir).unwrap();
 
   for f in it {
     // The into_string function will throw an error if it contains non-unicode characters
+    // TODO : Handle Error
     println!("{}", f.unwrap().file_name().into_string().unwrap());
   }
 }
 
-unsafe fn parse_args() -> Arguments<'static> {
+fn parse_args() -> Arguments {
   let mut args = env::args();
-  let mut parsed_args = Arguments { path: "." };
+  let mut parsed_args = Arguments { path: String::from(".") };
 
-    match args.next() {
-      Some(arg) => {
-        let path: &'static str = arg.as_str();
-        parsed_args.path = path;
-      },
-      None => println!("Arguments parsing done !")// There is no more argument
-    }
+  // Skip the 1st argument (executable name)
+  args.next();
+
+  // TODO : Check for Error (end of arguments)
+  let path = args.next().unwrap();
+  parsed_args.path = String::from(path.as_str());
 
   let final_args: Arguments = parsed_args;
+
   return final_args;
 }
 
 fn main() {
-  unsafe {
-    parse_args();
-  }
-//  print_dir(path);
+  // Gathering arguments
+  let args = parse_args();
+
+  // Passes reference to print_dir
+  print_dir(&args.path);
 }
