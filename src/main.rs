1cc89bc2250b5da62eab9a6e522a3ad477f09450
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-06-03 18:47:52 +0200

    Update comments

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,21 @@ use std::env;
 
 mod list;
 
+/// Print usage for the current program.
+///
+/// - program   : the current program name
+/// - opts      : array of options  to print
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
 }
 
-
-/**
- * First parses arguments.
- * Then run the print_dir function with the given arguments.
- */
 fn main() {
     // Must enforce type
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
+    // Build new Options parser
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
 
@@ -28,10 +28,9 @@ fn main() {
         Err(f) => panic!(f.to_string()),
     };
 
+    // Check for options
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return;
     }
-
-    // list::print_dir(&args.path);
 }
