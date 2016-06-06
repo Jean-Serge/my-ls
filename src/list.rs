7d030eb82267cd218c19f542c31e6885989629ed
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-06-06 19:06:43 +0200

    Enhance printing

--- src/list.rs
+++ src/list.rs
@@ -5,11 +5,13 @@ pub fn print_dir(dir: &String) -> () {
     // The unwrap function extracts the Ok() value of the Result
     let mut it = fs::read_dir(dir).unwrap();
 
+    println!("Listing {}", dir);
+
     loop {
         match it.next() {
             Some(file) => {
                 match file {
-                    Ok(f) => println!("{}", f.file_name().into_string().unwrap()),
+                    Ok(f) => println!("  {}", f.file_name().into_string().unwrap()),
                     Err(e) => println!("{}", e),
                 }
             }
