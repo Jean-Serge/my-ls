aed077e107900d93accc1e135eefac70de417dfd
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-06-10 08:55:11 +0200

    Add filter option to print_dir function

--- src/list.rs
+++ src/list.rs
@@ -1,17 +1,17 @@
 use std::fs;
+use std::io;
 
 /// Print the `dir_path` dir.
-pub fn print_dir(dir: &String) -> () {
-    // The unwrap function extracts the Ok() value of the Result
-    let mut it = fs::read_dir(dir).unwrap();
-
-    println!("Listing {}", dir);
+pub fn print_dir<F>(path: &String, filter: F) -> ()
+    where F : Fn(&Result<fs::DirEntry,io::Error>) -> bool {
+    let it = fs::read_dir(path).unwrap();
+    let mut filtered = it.filter(filter);
 
     loop {
-        match it.next() {
+        match filtered.next() {
             Some(file) => {
                 match file {
-                    Ok(f) => println!("  {}", f.file_name().into_string().unwrap()),
+                    Ok(f) => println!("{}", f.file_name().into_string().unwrap()),
                     Err(e) => println!("{}", e),
                 }
             }
