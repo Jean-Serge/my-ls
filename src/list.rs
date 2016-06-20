0c7638e5d70e8505b2247a27d96ad12bf91310de
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-06-20 08:35:38 +0200

    Add a few colors

--- src/list.rs
+++ src/list.rs
@@ -1,6 +1,32 @@
 use std::fs;
 use std::io;
 
+enum Color {
+    BLUE,
+    GREEN,
+    RED,
+    DEFAULT
+}
+
+fn color_to_bytes<'a>(c: Color) -> &'a str {
+    match c {
+        Color::BLUE => "\x1B[34m",
+        Color::GREEN => "\x1B[32m",
+        Color::RED => "\x1B[31m",
+        Color::DEFAULT => "\x1B[39m"
+    }
+}
+
+fn color_of_type<'a>(t: fs::FileType) -> &'a str {
+    if t.is_dir() {
+        color_to_bytes(Color::BLUE)
+    } else if t.is_file() {
+        color_to_bytes(Color::DEFAULT)
+    } else {
+        color_to_bytes(Color::GREEN)
+    }
+}
+
 /// Print the `dir_path` dir.
 pub fn print_dir<F>(path: &String, filter: F) -> ()
     where F: Fn(&Result<fs::DirEntry, io::Error>) -> bool
@@ -12,11 +38,23 @@ pub fn print_dir<F>(path: &String, filter: F) -> ()
         match filtered.next() {
             Some(file) => {
                 match file {
-                    Ok(f) => println!("{}", f.file_name().into_string().unwrap()),
+                    Ok(f) => print_file(f),
                     Err(e) => println!("{}", e),
                 }
             }
             None => break, // No more entries
         }
     }
+}
+
+/// Print a specific file
+fn print_file(f: fs::DirEntry) -> () {
+    let default = color_to_bytes(Color::DEFAULT);
+    let file_name = f.file_name().into_string().unwrap();
+    let color = match f.file_type() {
+        Ok(t) => color_of_type(t),
+        Err(_) => color_to_bytes(Color::RED)
+    };
+
+    println!("{}{}{}", color, file_name, default);
 }
